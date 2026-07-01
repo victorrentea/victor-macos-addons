3077c1d9322d3472fa04fd23ebc838283e029017
Victor Rentea <victorrentea@gmail.com>
2026-07-01 08:58:15 +0200

    Update Victor Addons build timestamp

--- Sources/VictorAddons/MenuBarManager.swift
+++ Sources/VictorAddons/MenuBarManager.swift
@@ -3,7 +3,7 @@ import Foundation
 import UserNotifications
 
 class MenuBarManager: NSObject, NSMenuDelegate {
-    static let BUILD_TIME = "Jul 1, 06:50"
+    static let BUILD_TIME = "Jul 1, 08:57"
 
     struct TranscriptionDebugState {
         let isTranscribing: Bool
