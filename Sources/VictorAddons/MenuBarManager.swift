b7047d7053f308e70d5f017c04be10ace55e7f48
Victor Rentea <victorrentea@gmail.com>
2026-07-29 00:52:31 +0300

    chore: bump BUILD_TIME stamp (build-app.sh)

--- Sources/VictorAddons/MenuBarManager.swift
+++ Sources/VictorAddons/MenuBarManager.swift
@@ -3,7 +3,7 @@ import Foundation
 import UserNotifications
 
 class MenuBarManager: NSObject, NSMenuDelegate {
-    static let BUILD_TIME = "Jul 29, 00:47"
+    static let BUILD_TIME = "Jul 29, 00:51"
 
     struct TranscriptionDebugState {
         let isTranscribing: Bool
