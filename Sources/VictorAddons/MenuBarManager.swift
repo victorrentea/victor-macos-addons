ba6d3ebf31c4fca55878515fd395b737b58b05f9
Victor Rentea <victorrentea@gmail.com>
2026-08-13 11:36:41 +0300

    docs: coffee shatter, the timer's fly-in, and the -1 tokens

--- Sources/VictorAddons/MenuBarManager.swift
+++ Sources/VictorAddons/MenuBarManager.swift
@@ -3,7 +3,7 @@ import Foundation
 import UserNotifications
 
 class MenuBarManager: NSObject, NSMenuDelegate {
-    static let BUILD_TIME = "Aug 13, 11:25"
+    static let BUILD_TIME = "Aug 13, 11:30"
 
     struct TranscriptionDebugState {
         let isTranscribing: Bool
