df5235e6da291a95777961de6936a37dfedeebed
Victor Rentea <victorrentea@gmail.com>
2026-08-14 22:48:58 +0300

    chore: build stamp

--- Sources/VictorAddons/MenuBarManager.swift
+++ Sources/VictorAddons/MenuBarManager.swift
@@ -3,7 +3,7 @@ import Foundation
 import UserNotifications
 
 class MenuBarManager: NSObject, NSMenuDelegate {
-    static let BUILD_TIME = "Aug 14, 22:11"
+    static let BUILD_TIME = "Aug 14, 22:48"
 
     struct TranscriptionDebugState {
         let isTranscribing: Bool
